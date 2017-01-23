87c958658ef57baccd32cfeec95150d4ae0b70f9
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-23 14:36:08 +0100

    fix lib python 2

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo "initiate environnement test"
 sudo apt-get update -yqq
 sudo apt-get install python3-dev -yqq
-sudo apt-get install python2-dev -yqq
+sudo apt-get install libpython-dev -yqq
 sudo apt-get install postgresql -yqq
 sudo pip install -r requirements.txt 
 echo "------------------------------"
