930f88a2c9ced4a255370e0945c9cd2d82ce95c9
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-27 11:59:39 +0100

    fix IT hello bob

--- github/trufflehog.py
+++ github/trufflehog.py
@@ -10,15 +10,17 @@ import stat
 from git import Repo
 
 if sys.version_info[0] == 2:
-    reload(sys)  
+    reload(sys)
     sys.setdefaultencoding('utf8')
 
 BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 HEX_CHARS = "1234567890abcdefABCDEF"
 
+
 def del_rw(action, name, exc):
     os.chmod(name, stat.S_IWRITE)
     os.remove(name)
+
 
 def shannon_entropy(data, iterator):
     """
@@ -28,9 +30,9 @@ def shannon_entropy(data, iterator):
         return 0
     entropy = 0
     for x in (ord(c) for c in iterator):
-        p_x = float(data.count(chr(x)))/len(data)
+        p_x = float(data.count(chr(x))) / len(data)
         if p_x > 0:
-            entropy += - p_x*math.log(p_x, 2)
+            entropy += - p_x * math.log(p_x, 2)
     return entropy
 
 
@@ -51,6 +53,7 @@ def get_strings_of_set(word, char_set, threshold=20):
         strings.append(letters)
     return strings
 
+
 class bcolors:
     EOL = '<br/>'
     OKBLUE = '<font color="blue">'
@@ -60,14 +63,12 @@ class bcolors:
     ENDC = '</font>'
 
 
-
 def find_strings(git_url):
     project_path = tempfile.mkdtemp()
 
     Repo.clone_from(git_url, project_path)
 
     repo = Repo(project_path)
-
 
     already_searched = set()
     for remote_branch in repo.remotes.origin.fetch():
@@ -76,13 +77,13 @@ def find_strings(git_url):
             repo.git.checkout(remote_branch, b=branch_name)
         except:
             pass
-     
+
         prev_commit = None
         for curr_commit in repo.iter_commits():
             if not prev_commit:
                 pass
             else:
-                #avoid searching the same diffs
+                # avoid searching the same diffs
                 hashes = str(prev_commit) + str(curr_commit)
                 if hashes in already_searched:
                     prev_commit = curr_commit
@@ -92,7 +93,7 @@ def find_strings(git_url):
                 diff = prev_commit.diff(curr_commit, create_patch=True)
 
                 for blob in diff:
-                    #print i.a_blob.data_stream.read()
+                    # print i.a_blob.data_stream.read()
                     printable_diff = blob.diff.decode()
                     if printable_diff.startswith("Binary files"):
                         continue
@@ -107,31 +108,31 @@ def find_strings(git_url):
                                 b64_entropy = shannon_entropy(string, BASE64_CHARS)
                                 if b64_entropy > 4.5:
                                     found_something = True
-                                    printable_diff = printable_diff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                                    printable_diff = printable_diff.replace(string,
+                                                                            bcolors.WARNING + string + bcolors.ENDC)
                             for string in hex_strings:
                                 hex_entropy = shannon_entropy(string, HEX_CHARS)
                                 if hex_entropy > 3:
                                     found_something = True
-                                    printable_diff = printable_diff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
+                                    printable_diff = printable_diff.replace(string,
+                                                                            bcolors.WARNING + string + bcolors.ENDC)
                     if found_something:
-                        commit_time = datetime.datetime.fromtimestamp(prev_commit.committed_date).strftime('%Y-%m-%d %H:%M:%S')
+                        commit_time = datetime.datetime.fromtimestamp(prev_commit.committed_date).strftime(
+                            '%Y-%m-%d %H:%M:%S')
                         print(bcolors.EOL)
                         print(bcolors.OKGREEN + "Date: " + commit_time + bcolors.ENDC)
                         print(bcolors.OKGREEN + "Branch: " + branch_name + bcolors.ENDC)
-                        print(bcolors.OKGREEN + "Commit: " + prev_commit.message + bcolors.ENDC)
+                        print(bcolors.OKGREEN + "Commit: " + prev_commit.message + bcolors.ENDC + bcolors.EOL)
                         print(printable_diff)
-                    
+
             prev_commit = curr_commit
     return project_path
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find secrets hidden in the depths of git.')
     parser.add_argument('git_url', type=str, help='URL for secret searching')
 
-
     args = parser.parse_args()
     project_path = find_strings(args.git_url)
     shutil.rmtree(project_path, onerror=del_rw)
-
-
-
