374b8c35908959a7f0e41531ab1f87e58e636dfe
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-30 10:13:43 +0100

    install py3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "initiate environnement test"
 sudo apt-get update -yqq
+sudo apt-get install python3 -yqq
 sudo apt-get install python3-dev -yqq
+sudo apt-get install python2 -yqq
 sudo apt-get install libpython-dev -yqq
 sudo apt-get install postgresql -yqq
 sudo pip install -r requirements.txt
