247b1c0090c476bf1378d2b0b05aa10d348ad685
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-03 17:27:19 +0100

    fix IT hello bob

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@
 #3 : registry docker ex (%docker_registry%)
 #4 : build branch ex (%teamcity.build.branch%)
 #####################################################################
-tag=$(echo utils/docker/extract_tag.sh "$4")
+tag=$(sh utils/docker/extract_tag.sh "$4")
 docker login $2 https://$3
 cd artifacts
 imagename=$(cat docker/artifact.yml | shyaml get-value image.name)
