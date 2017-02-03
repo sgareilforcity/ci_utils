79db2b880e4adc00c1f080ada719f9a8f89275b7
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-03 14:36:59 +0100

    fix IT hello bob

--- docker/fetch.sh
+++ docker/fetch.sh
@@ -3,10 +3,12 @@
 #2 : credential docker ex (-u %docker_login% -p "%docker_password%")
 #3 : registry docker ex (%docker_registry%)
 #4 : build branch ex (%teamcity.build.branch%)
+
 ls -lisa
+
 docker login $2 https://$3
-chmod -x extract_tag.sh
-tag= $(sh extract_tag.sh $4)
+
+tag= $(sh utils/docker/extract_tag.sh $4)
 echo "fetching $3/$1:$tag"
 docker pull $3/$1:$tag
 
