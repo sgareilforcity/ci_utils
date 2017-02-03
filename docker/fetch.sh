0e832b2e9bcf1e5e0fd2a78de8e35b00527e0ba6
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-03 14:53:22 +0100

    fix IT hello bob

--- docker/fetch.sh
+++ docker/fetch.sh
@@ -8,8 +8,13 @@ ls -lisa
 
 docker login $2 https://$3
 
-tag= $(sh utils/docker/extract_tag.sh $4)
+tag=$(echo sh utils/docker/extract_tag.sh $4)
 echo "fetching $3/$1:$tag"
 docker pull $3/$1:$tag
-
+result=$?
+if [ "$result" != "0" ]; then
+    echo "##teamcity[message text='Error while fetching image $imagename' errorDetails='Error while building image $imagename' status='ERROR']"
+    echo "Error while building $imagename"
+    exit 418
+fi
 exit 0
