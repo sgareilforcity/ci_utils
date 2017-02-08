ebde9348977a849cb15b1d4ea3e5d07369cfa888
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-08 16:11:56 +0100

    fix IT hello bob

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo apt-get update -yqq
 sudo apt-cache show python
 sudo apt-get install python2.7 -yqq
 sudo apt-get install libpython-dev -yqq
-#sudo apt-get install python3.5 -yqq
-sudo apt-get install python3=3.5.3* -yqq
+sudo apt-get install python3.5 -yqq
+#sudo apt-get install python3=3.5.3* -yqq
 sudo apt-get install postgresql -yqq
 sudo pip install -r requirements.txt
 sudo pip install shyaml -q
