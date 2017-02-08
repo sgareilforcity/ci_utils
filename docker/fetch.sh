a3e8da5e73b2fbb9f4f3ea87b896793ad5353c7c
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-08 17:17:00 +0100

    fix push docker

--- docker/fetch.sh
+++ docker/fetch.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# exemple :sh fetch.sh platform/ci/template -u teamcity -p ******* registry-v2.forcity.io refs/tags/1.0.0-alpha
+# exemple :sh fetch.sh -u teamcity -p ******* registry-v2.forcity.io refs/tags/1.0.0-alpha
 ####################################################################
-#1 : image name of the artifact
-#2 : credential docker ex (-u %docker_login% -p "%docker_password%")
-#3 : registry docker ex (%docker_registry%)
-#4 : build branch ex (%teamcity.build.branch%)
+#1 : credential docker ex (-u %docker_login% -p "%docker_password%")
+#2 : registry docker ex (%docker_registry%)
+#3 : build branch ex (%teamcity.build.branch%)
 #####################################################################
 image_name=$(cat artifacts/docker/artifact.yml | shyaml get-value image.name)
-docker login $2 https://$3
-tag=$(sh utils/docker/extract_tag.sh $4)
-echo "##teamcity[progressMessage 'fetching $3/$1:$tag']"
-docker pull $3/$1:$tag
+docker login $1 https://$2
+tag=$(sh utils/docker/extract_tag.sh $3)
+echo "##teamcity[progressMessage 'fetching $2/$image_name:$tag']"
+docker pull $2/$image_name:$tag
 sh utils/teamcity/teamcity_error.sh $? 0
 
 
--- docker/push.sh
+++ docker/push.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
-
-catalog=$(cat catalog)
-docker login -u %docker_login% -p "%docker_password%" https://%docker_registry%
-dependency_branch=$(echo "%dep.Lumis_Lumis.teamcity.build.branch%" | sed "s,refs/heads/,,g")
-for imagename in $catalog; do
-    imagepath=$(echo $imagename|sed "s,@,/,g")
-    imagename=$(echo $imagename|sed "s/@/_/g")
-    if [ -d "$imagepath" ]; then
-        if [ "$dependency_branch" == "master" ]; then
-            version="latest"
-            tags="latest build-%build.number%"
-        else
-            version=$(echo "$dependency_branch" | sed "s,release/,,g" | sed "s,/,-,g")
-            tags="$version"
-            echo "Processing $imagename:$tags...."
-        fi
-        for tag in $tags; do
-            echo "Pushing %docker_registry%/$imagename:$tag"
-            docker push %docker_registry%/$imagename:$tag
-            result=$?
-            if [ "$result" != "0" ];then
-                echo "Error while pushing."
-                exit 1
-            fi
-        done
-    else
-      echo "Cannot find $imagepath"
-    fi
-
-done
+####################################################################
+#1 : credential docker ex (-u %docker_login% -p "%docker_password%")
+#2 : registry docker ex (%docker_registry%)
+#3 : build branch ex (%teamcity.build.branch%)
+#####################################################################
+image_name=$(cat artifacts/docker/artifact.yml | shyaml get-value image.name)
+docker login $1 https://$2
+tag=$(sh utils/docker/extract_tag.sh $3)
+cd artifacts
+docker push %docker_registry%/$imagename:$tag
+result=$?
+cd ..
+sh utils/teamcity/teamcity_error.sh $? 1 ERROR
