e03182f44d02364db40e9815403c4378237c9015
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-08 11:59:44 +0100

    fix teamcity message error and warn

--- teamcity/teamcity_error.sh
+++ teamcity/teamcity_error.sh
@@ -2,13 +2,19 @@
 # 1 : result=$?
 # 2 : new exit code
 # 3 : status
-if[ -z $3 ]; then
-    status=$3
+if [ -z $1 ] ; then
+   echo "# 1 : result=$?"
+   echo "# 2 : new exit code"
+   echo "# 3 : status"
+   exit 418
 else
-    status='WARN'
+    status=$3
+    if [ -z $status ] ; then
+       status='WARN'
+    fi
+    if [ "$1" != "0" ]; then
+        echo "##teamcity[message text='Error while fetching image $imagename' errorDetails='Error while fetching image $imagename' status='$status']"
+        return $2
+    fi
+    return $1
 fi
-if [ "$1" != "0" ]; then
-    echo "##teamcity[message text='Error while fetching image $imagename' errorDetails='Error while fetching image $imagename' status='$status']"
-    return $2
-fi
-return $1
