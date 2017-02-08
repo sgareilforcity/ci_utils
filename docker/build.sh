eb15d4f1938a8916360d9b1e6c00811767c6ebdf
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-08 17:21:15 +0100

    fix IT hello bob

--- docker/build.sh
+++ docker/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Log into docker registry
 ####################################################################
-#1 : image name of the artifact
-#2 : credential docker ex (-u %docker_login% -p "%docker_password%")
-#3 : registry docker ex (%docker_registry%)
-#4 : build branch ex (%teamcity.build.branch%)
+#1 : credential docker ex (-u %docker_login% -p "%docker_password%")
+#2 : registry docker ex (%docker_registry%)
+#3 : build branch ex (%teamcity.build.branch%)
 #####################################################################
-tag=$(sh utils/docker/extract_tag.sh "$4")
-docker login $2 https://$3
+image_name=$(cat artifacts/docker/artifact.yml | shyaml get-value image.name)
+docker login $1 https://$2
+tag=$(sh utils/docker/extract_tag.sh $3)
 cd artifacts
-imagename=$(cat docker/artifact.yml | shyaml get-value image.name)
-echo "##teamcity[progressMessage 'Building image $3/$imagename:$tag']"
-docker build -t $3/$imagename:$tag -f  docker/Dockerfile .
+echo "##teamcity[progressMessage 'Building image $2/$imagename:$tag']"
+docker build -t $2/$imagename:$tag -f  docker/Dockerfile .
 result=$?
 cd ..
 sh utils/teamcity/teamcity_error.sh $result 418
