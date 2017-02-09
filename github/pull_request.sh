ec6d0baa2154999c724560d9bc6ecc70eabc5c1a
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-09 17:52:13 +0100

    fix IT hello bob

--- github/pull_request.sh
+++ github/pull_request.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+# utils/github/pull_request.sh
 # exemple :sh fetch.sh -u teamcity -p ******* registry-v2.forcity.io refs/tags/1.0.0-alpha
 ####################################################################
 #1 : build branch ex (%teamcity.build.branch%)
-#2 : repo git ex (sgareilforcity/)
+#2 : repo git ex (sgareilforcity/template_ci)
+#3 : access token git
 #####################################################################
 image_name=$(cat artifacts/docker/artifact.yml | shyaml get-value image.name)
-tag=$(sh utils/docker/extract_tag.sh $3)
+tag=$(sh utils/docker/extract_tag.sh $1)
 echo "##teamcity[progressMessage 'pull requesting on Master of $1']"
 
 if[ $1 = "refs/heads/release-candidate" ];then
-curl post {
+curl post -H "Content-Type: application/json" -H "authToken: $3"  --data '{
   "title": "Amazing new feature",
   "body": "Please pull this in!",
-  "head": "sgareilforcity:release-candidate",
+  "head": "release-candidate",
   "base": "master"
-}
+}' https://github.com/api/repos/$2/pulls
 fi
 exit 0
